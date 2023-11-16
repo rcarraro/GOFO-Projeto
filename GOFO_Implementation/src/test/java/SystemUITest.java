3dee8636f49e079aafb74db8407ae195aa72a1c2
Rcarraro <carrarorafa@gmail.com>
2023-11-16 20:23:45 -0300

    teste

--- GOFO_Implementation/src/test/java/SystemUITest.java
+++ GOFO_Implementation/src/test/java/SystemUITest.java
@@ -1,85 +1,38 @@
-import codigo.System.Administrator;
-import codigo.System.Player;
-import codigo.System.PlaygroundOwner;
-import codigo.System.PlaygroundOwner;
-import codigo.UI.SystemUI;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.Rule;
 import org.junit.contrib.java.lang.system.TextFromStandardInputStream;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
+import org.junit.contrib.java.lang.system.SystemOutRule;
 import java.io.InputStream;
-import java.io.PrintStream;
-import java.util.ArrayList;
-
 import static org.junit.Assert.assertEquals;
 
 public class SystemUITest {
 
     private final InputStream originalSystemIn = System.in;
-    private final PrintStream originalSystemOut = System.out;
+    private final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
     private SystemUI sistema;
+
     @Before
-    public void setUp(){
+    public void setUp() {
         sistema = new SystemUI();
     }
 
     @Rule
     public final TextFromStandardInputStream systemIn = TextFromStandardInputStream.emptyStandardInputStream();
 
-    // @Test
-    // public void testRegisterPlayer() {
-    //     systemIn.provideLines("John","Doe","123","password","john.doe@example.com","456","City","player");
-    //     sistema.register(); 
-    // }
+    @Rule
+    public final ExpectedSystemExit exit = ExpectedSystemExit.none();
+
     @Test
-    public void testaccountMenuLoginsemcadastro(){
-        PrintStream originalOut = System.out;
+    public void testaccountMenuLoginsemcadastro() {
         systemIn.provideLines("1", "rafael@fei.com.br", "123456");
         sistema.accountMenu();
+        assertEquals("Expected output", "Your custom expected output here\n", systemOutRule.getLog());
     }
 
-    // @Test
-    // public void testLoginPlayer() {
-    //     Player player = new Player();
-    //     player.setEmail("john.doe@example.com");
-    //     player.setPassword("password");
-    //     ArrayList<Player> players = new ArrayList<>();
-    //     players.add(player);
-
-    //     sistema.thePlayers = players;
-
-    //     String simulatedInput = "john.doe@example.com\npassword\n";
-    //     provideInput(simulatedInput);
-
-    //     sistema.Login();
-
-    //     assertEquals("Logged in successfully\n", outputStream.toString());
-    // }
-
-    // @Test
-    // public void testComplaintForm() {
-    //     Administrator admin = new Administrator();
-    //     sistema.admin = admin;
-
-    //     PlaygroundOwner owner = new PlaygroundOwner();
-    //     owner.setEmail("owner@example.com");
-    //     ArrayList<PlaygroundOwner> owners = new ArrayList<>();
-    //     owners.add(owner);
-    //     sistema.theOwners = owners;
-
-    //     String simulatedInput = "playground owner\nowner@example.com\nComplaint about the owner\n";
-    //     provideInput(simulatedInput);
-
-    //     sistema.complaintForm();
-
-    //     assertEquals("User found!!\n", outputStream.toString());
-    // }
-
-    // private void provideInput(String data) {
-    //     ByteArrayInputStream inputStream = new ByteArrayInputStream(data.getBytes());
-    //     System.setIn(inputStream);
-    // }
+    @After
+    public void restoreSystemInputOutput() {
+        System.setIn(originalSystemIn);
+    }
 }
