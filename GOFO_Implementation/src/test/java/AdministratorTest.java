ea28fdb500465b3a46b68807eb277770f91c66ca
Rcarraro <carrarorafa@gmail.com>
2023-11-16 23:52:43 -0300

    teste

--- GOFO_Implementation/src/test/java/AdministratorTest.java
+++ GOFO_Implementation/src/test/java/AdministratorTest.java
@@ -1,8 +1,12 @@
-import codigo.System.Administrator;
-import codigo.System.Playground;
+import codigo.UI.SystemUI;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
+import org.junit.Rule;
+import org.junit.contrib.java.lang.system.TextFromStandardInputStream;
+import org.junit.contrib.java.lang.system.SystemOutRule;
+import org.junit.contrib.java.lang.system.ExpectedSystemExit;
+import java.io.InputStream;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
