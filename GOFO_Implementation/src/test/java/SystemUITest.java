46c606b54e442ec99c50c65b0e0f2180bd16529e
Rcarraro <carrarorafa@gmail.com>
2023-11-18 10:46:15 -0300

    teste

--- GOFO_Implementation/src/test/java/SystemUITest.java
+++ GOFO_Implementation/src/test/java/SystemUITest.java
@@ -29,11 +29,10 @@ public class SystemUITest {
 
     @Test
     public void testaccountMenuLoginsemcadastro() {
-        exit.expectSystemExitWithStatus(0);
         // systemIn.provideLines("1", "rafael@fei.com.br", "123456","2", "Rafael", "Martins", "123", "123456", "rafael@fei.com.br", "12345678", "SP", "player","1234" ,"123", "1", "rafael@fei.com.br", "123456", "12", "3");
         systemIn.provideLines("1");
         sistema.accountMenu();
-        // assertTrue("Verificação", actualOutput.contains("Logout"));
+        exit.expectSystemExitWithStatus(0);
     }
 
     @After
