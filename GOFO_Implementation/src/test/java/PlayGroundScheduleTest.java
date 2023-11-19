5c11d9c0b1f5b2af16bf8e63b2e3c4bc0026d064
Rcarraro <carrarorafa@gmail.com>
2023-11-18 21:19:19 -0300

    ajuste nos testes

--- GOFO_Implementation/src/test/java/PlayGroundScheduleTest.java
+++ GOFO_Implementation/src/test/java/PlayGroundScheduleTest.java
@@ -11,7 +11,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 
-public class PlaygroundTest {
+public class PlayGroundScheduleTest {
 
     private final InputStream originalSystemIn = System.in;
     private final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
