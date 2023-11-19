6a87900b35c818d07b44e6b14045ad6f5c5eb9ca
Rcarraro <carrarorafa@gmail.com>
2023-11-19 18:25:50 -0300

    ajuste nos testes

--- GOFO_Implementation/src/test/java/US01Test.java
+++ GOFO_Implementation/src/test/java/US01Test.java
@@ -32,9 +32,9 @@ public class US01Test {
 
     @Test
     public void testaccountMenuLoginsemcadastro() {
+        exit.expectSystemExitWithStatus(0);
         systemIn.provideLines("2","Kleber","Silva","123","123456","teste@fei.edu.br", "123456789", "SP", "player", "123", "3");
         sistema.accountMenu();
-        exit.expectSystemExitWithStatus(0);
     }
 
     @After
