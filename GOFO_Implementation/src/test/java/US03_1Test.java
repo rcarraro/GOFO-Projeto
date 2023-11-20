24a2f3f606f33a8a6344d3fb0e7ee5e505e1e200
Rcarraro <carrarorafa@gmail.com>
2023-11-20 18:59:19 -0300

    ajuste nos testes de caso de uso US03_1

--- GOFO_Implementation/src/test/java/US03_1Test.java
+++ GOFO_Implementation/src/test/java/US03_1Test.java
@@ -40,7 +40,7 @@ public class US03_1Test {
 
     @Test
     public void testaccountMenu() {
-        exit.expectSystemExitWithStatus(0);
+        exit.expectSystemExitWithStatus(0); 
         systemIn.provideLines("2", "test", "Silva","123", "123456", "test@fei.br", "123456789", "SP", "player", "10000", "123", "1", "test@fei.br", "123456", "5", "teste", "5", "0", "monday", "5", "12", "3");
         sistema.accountMenu();
     }
