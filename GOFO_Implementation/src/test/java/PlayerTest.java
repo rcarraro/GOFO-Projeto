7178b3ca12d8f581c8ba36fda1e946c3456e8e59
Amanda de Sousa Martins <91508134+AmuHeartifilia@users.noreply.github.com>
2023-11-20 17:24:21 -0300

    Update PlayerTest.java

--- GOFO_Implementation/src/test/java/PlayerTest.java
+++ GOFO_Implementation/src/test/java/PlayerTest.java
@@ -1,1 +1,39 @@
+import codigo.System.PlayGroundSchedule;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Rule;
+import org.junit.contrib.java.lang.system.TextFromStandardInputStream;
+import org.junit.contrib.java.lang.system.SystemOutRule;
+import org.junit.contrib.java.lang.system.ExpectedSystemExit;
+import java.io.InputStream;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
 
+public class PlayerTest {
+
+     private final InputStream originalSystemIn = System.in;
+     private final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
+
+     @Rule
+     public final TextFromStandardInputStream systemIn = TextFromStandardInputStream.emptyStandardInputStream();
+
+     @Rule
+     public final ExpectedSystemExit exit = ExpectedSystemExit.none();
+
+     Player player;
+   
+     //@Before
+     //public void setUp() {
+     //    playgrounds = new PlayGroundSchedule();
+     //    assertEquals(5, playgrounds.getDayIndex("thursday"));
+     //}
+
+     @Test
+     public void testSetBalance(){
+         player.setBalance(10)
+         assertEquals(10, player.getBalance(10));
+     }
+
+}
