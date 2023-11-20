2daf191b58c58be459e396b80333a588ec962173
LeoMarinaro <97569362+LeoMarinaro@users.noreply.github.com>
2023-11-20 18:53:32 -0300

    Update PlaygroundOwnerTest.java

--- GOFO_Implementation/src/test/java/PlaygroundOwnerTest.java
+++ GOFO_Implementation/src/test/java/PlaygroundOwnerTest.java
@@ -1,1 +1,54 @@
+import codigo.System.PlaygroundOwner;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Rule;
+import org.junit.contrib.java.lang.system.TextFromStandardInputStream;
+import org.junit.contrib.java.lang.system.SystemOutRule;
+import org.junit.contrib.java.lang.system.ExpectedSystemExit;
+import java.io.InputStream;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
 
+public class PlaygroundOwnerTest {
+
+    @Rule
+    public final TextFromStandardInputStream systemIn = TextFromStandardInputStream.emptyStandardInputStream();
+
+    @Rule
+    public final ExpectedSystemExit exit = ExpectedSystemExit.none();
+
+    PlaygroundOwner playgroundowner;
+    Playground playground;
+  
+    @Before
+    public void setUp() {
+        playground = new Playground();
+        playgroundowner = new PlaygroundOwner();
+
+    }
+
+    @Test
+    public void testaddPlayground(){
+        playgroundowner.addPlayground(playground);
+    }
+
+    @Test
+    public void testexistPlayground(){
+        assertEquals(false, playgroundowner.existPlayground("name");
+    }
+
+   @Test
+    public void testupdatePlaygroundName(){
+        assertEquals(false, playgroundowner.updatePlaygroundName("name");
+    }
+
+
+
+
+    @After
+    public void restoreSystemInputOutput() {
+        System.setIn(originalSystemIn);
+    }
+}
