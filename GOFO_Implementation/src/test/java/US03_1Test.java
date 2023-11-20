8b8fc44c7bd78abb6697f47be8b54a98de7ba694
Rcarraro <carrarorafa@gmail.com>
2023-11-20 19:44:18 -0300

    ajuste nos testes de caso de uso US03_1

--- GOFO_Implementation/src/test/java/US03_1Test.java
+++ GOFO_Implementation/src/test/java/US03_1Test.java
@@ -49,7 +49,7 @@ public class US03_1Test {
         playgroundo.setPhone(124356789);
         playgroundo.setEmail("test@fei.edu.br");
         playgroundo.setLocation("SP");
-        theOwners.add(playgroundo);
+        sistema.theOwners.add(playgroundo);
     }
 
     @Test
