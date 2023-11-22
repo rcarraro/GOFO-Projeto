c8f304017a700894eadf471370c3dae1bed0352c
Rcarraro <carrarorafa@gmail.com>
2023-11-21 23:25:36 -0300

    ajuste nos testes de caso de uso US05_1

--- GOFO_Implementation/src/test/java/US05_1Test.java
+++ GOFO_Implementation/src/test/java/US05_1Test.java
@@ -68,7 +68,7 @@ public class US05_1Test {
     public void testaccountMenu() {
         exit.expectSystemExitWithStatus(0);
         int valor = playown.getMyBalance();
-        systemIn.provideLines("2", "test", "Silva","123", "123456", "test@fei.br", "123456789", "SP", "player", "10000", "123", "1", "test@fei.br", "123456"); 
+        systemIn.provideLines("2", "test", "Silva","123", "123456", "test@fei.br", "123456789", "SP", "player", "10000", "123", "1", "test@fei.br", "123456", "123456"); 
         // systemIn.provideLines("2", "test", "Silva","123", "123", "test@fei.br", "123456789", "SP", "player", "10000", "123", "1", "test@fei.br", "123", "10", "ateste", "3", "2", "ateste", "3", "5", "monday"); 
         sistema.accountMenu();
         // assertEquals(valor+500, playown.getMyBalance());
