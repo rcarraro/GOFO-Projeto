5c1d7aaad8f3265afd6c5e6d8345bb4805aec1fe
Rcarraro <carrarorafa@gmail.com>
2023-11-22 19:32:23 -0300

    ajuste nos testes de caso de uso US07_1

--- GOFO_Implementation/src/test/java/US07_2Test.java
+++ GOFO_Implementation/src/test/java/US07_2Test.java
@@ -18,7 +18,7 @@
 // import static org.junit.Assert.assertThrows;
 // import static org.junit.Assert.assertTrue;
 
-// public class US06_2Test {
+// public class US07_2Test {
 
 //     private final InputStream originalSystemIn = System.in;
 //     private final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
