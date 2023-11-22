acef582c3bbbd902fe71725b881757a3bf23ff67
Rcarraro <carrarorafa@gmail.com>
2023-11-21 22:17:03 -0300

    ajuste nos testes de caso de uso US04_2

--- GOFO_Implementation/src/test/java/US04_2Test.java
+++ GOFO_Implementation/src/test/java/US04_2Test.java
@@ -1,66 +1,97 @@
-// import codigo.UI.SystemUI;
-// import codigo.System.Playground;
-// import codigo.System.eWallet;
-// import codigo.System.PlaygroundOwner;
-// import org.junit.After;
-// import org.junit.Before;
-// import org.junit.Test;
-// import org.junit.Rule;
-// import org.junit.contrib.java.lang.system.TextFromStandardInputStream;
-// import org.junit.contrib.java.lang.system.SystemOutRule;
-// import org.junit.contrib.java.lang.system.ExpectedSystemExit;
-// import java.io.InputStream;
+import codigo.UI.SystemUI;
+import codigo.System.Playground;
+import codigo.System.Administrator;
+import codigo.System.Player;
+import codigo.System.eWallet;
+import codigo.System.PlaygroundOwner;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Rule;
+import org.junit.contrib.java.lang.system.TextFromStandardInputStream;
+import org.omg.PortableInterceptor.PolicyFactoryOperations;
+import org.junit.contrib.java.lang.system.SystemOutRule;
+import org.junit.contrib.java.lang.system.ExpectedSystemExit;
+import java.io.InputStream;
 
-// import static org.junit.Assert.assertEquals;
-// import static org.junit.Assert.assertThrows;
-// import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
 
-// public class US04_2Test {
+public class US04_2Test {
 
-//     private final InputStream originalSystemIn = System.in;
-//     private final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
+    private final InputStream originalSystemIn = System.in;
+    private final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
 
-//     @Rule
-//     public final TextFromStandardInputStream systemIn = TextFromStandardInputStream.emptyStandardInputStream();
+    @Rule
+    public final TextFromStandardInputStream systemIn = TextFromStandardInputStream.emptyStandardInputStream();
 
-//     @Rule
-//     public final ExpectedSystemExit exit = ExpectedSystemExit.none();
+    @Rule
+    public final ExpectedSystemExit exit = ExpectedSystemExit.none();
 
-//     private SystemUI sistema;
-//     private Playground playground;
-//     private PlaygroundOwner playgroundo;
-//     eWallet serve;
+    private SystemUI sistema;
+    private Playground playground1;
+    private Playground playground2;
+    private Playground playground3;
+    private Administrator administrator;
+    Player player;
    
-//     @Before
-//     public void setUp() {
-//         sistema = new SystemUI();
-//         serve = new eWallet();
-//         playground = new Playground();
-//         playgroundo = new PlaygroundOwner();
-//         playground.setName("teste");
-//         serve.setBalance(10000);
-//         playgroundo.setBalance(serve);
-//         playgroundo.setFName("Fname");
-//         playgroundo.setLName("lName");
-//         playgroundo.setPassword("password");
-//         playgroundo.setID(123);
-//         playgroundo.setRule("playground owner");
-//         playgroundo.setPhone(124356789);
-//         playgroundo.setEmail("test@fei.edu.br");
-//         playgroundo.setLocation("SP");
-//         playgroundo.addPlayground(playground);
-//         sistema.theOwners.add(playgroundo);
-//     }
-
-//     @Test
-//     public void testaccountMenu() {
-//         exit.expectSystemExitWithStatus(0);
-//         systemIn.provideLines("2", "test", "Silva","123", "123456", "test@fei.br", "123456789", "SP", "player", "10000", "123", "1", "test@fei.br", "123456", "5", "teste", "0", "0", "monday", "0", "12", "3");
-//         sistema.accountMenu();
-//     }
+    @Before
+    public void setUp() {
+        sistema = new SystemUI();
+        administrator = new Administrator();
+        playground1 = new Playground();
+        playground2 = new Playground();
+        playground3 = new Playground();
+        player = new Player();
+        administrator.playgroundRequests(playground1);
+        administrator.playgroundRequests(playground2);
+        administrator.playgroundRequests(playground3);
+        
+        //locais
+        systemIn.provideLines("SP");
+        playground1.setLocation();
+        systemIn.provideLines("RJ");
+        playground2.setLocation();
+        systemIn.provideLines("MT");
+        playground2.setLocation();
+        //aprovar
+        systemIn.provideLines("yes","yes", "yes");
+        administrator.approvePlayground();
+        //Nomes
+        playground1.setName("ateste");
+        playground2.setName("bteste");
+        playground3.setName("cteste");
+        //Preço
+        systemIn.provideLines("100");
+        playground1.setPrice();
+        systemIn.provideLines("200");
+        playground2.setPrice();
+        systemIn.provideLines("250");
+        playground3.setPrice();
+        //status
+        systemIn.provideLines("available");
+        playground1.setStatus();
+        systemIn.provideLines("not available");
+        playground2.setStatus();
+        systemIn.provideLines("available");
+        playground3.setStatus();
+        systemIn.provideLines("0","10");
+        playground1.setBooking();
+    }
     
-//     @After
-//     public void restoreSystemInputOutput() {
-//         System.setIn(originalSystemIn);
-//     }
-// }
+    @Test
+    public void testaccountMenu() {
+        
+        exit.expectSystemExitWithStatus(0);
+        systemIn.provideLines("2", "test", "Silva","123", "123456", "test@fei.br", "123456789", "SP", "player", "10000", "123", "1", "test@fei.br", "123456", "3", "2", "bteste"); 
+        player.addInbox("Você foi convidado para o playground ateste");
+        sistema.accountMenu();
+        // systemIn.provideLines("2", "test", "Silva","123", "123456", "test@fei.br", "123456789", "SP", "player", "10000", "123", "1", "test@fei.br", "123456", "10", "ateste","3","2", "ateste", "0", "0", "asd", "sunday"); 
+    }
+    
+    @After
+    public void restoreSystemInputOutput() {
+        System.setIn(originalSystemIn);
+    }
+}
