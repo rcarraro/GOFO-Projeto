901fc569ad6aa73cc65eb5979474c64f6776c4da
Rcarraro <carrarorafa@gmail.com>
2023-11-21 21:41:01 -0300

    ajuste nos testes de caso de uso US04_1

--- GOFO_Implementation/src/test/java/US04_1Test.java
+++ GOFO_Implementation/src/test/java/US04_1Test.java
@@ -81,7 +81,7 @@ public class US04_1Test {
     @Test
     public void testaccountMenu() {
         exit.expectSystemExitWithStatus(0);
-        systemIn.provideLines("2", "test", "Silva","123", "123456", "test@fei.br", "123456789", "SP", "player", "10000", "123", "1", "test@fei.br", "123456", "10", "ateste", "3", "2", "ateste", "9"); 
+        systemIn.provideLines("2", "test", "Silva","123", "123456", "test@fei.br", "123456789", "SP", "player", "10000", "123", "1", "test@fei.br", "123456", "10", "ateste", "3", "2", "ateste", "9", "10", "sunday"); 
         // systemIn.provideLines("2", "test", "Silva","123", "123456", "test@fei.br", "123456789", "SP", "player", "10000", "123", "1", "test@fei.br", "123456", "10", "ateste","3","2", "ateste", "0", "0", "asd", "sunday"); 
         sistema.accountMenu();
     }
