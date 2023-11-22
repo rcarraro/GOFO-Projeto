0f13f3bb08fe84bee506c266aed21e8bb96f0365
Rcarraro <carrarorafa@gmail.com>
2023-11-21 22:22:27 -0300

    ajuste nos testes de caso de uso US04_2

--- GOFO_Implementation/src/test/java/US04_2Test.java
+++ GOFO_Implementation/src/test/java/US04_2Test.java
@@ -86,10 +86,9 @@ public class US04_2Test {
     public void testaccountMenu() {
         
         exit.expectSystemExitWithStatus(0);
-        systemIn.provideLines("2", "test", "Silva","123", "123456", "test@fei.br", "123456789", "SP", "player", "10000", "123", "1", "test@fei.br", "123456", "3", "2", "bteste"); 
+        systemIn.provideLines("2", "test", "Silva","123", "123456", "test@fei.br", "123456789", "SP", "player", "10000", "123", "1", "test@fei.br", "123456", "3", "2", "bteste", "3", "4", "12", "3"); 
         player.addInbox("Você foi convidado para o playground ateste");
         sistema.accountMenu();
-        // systemIn.provideLines("2", "test", "Silva","123", "123456", "test@fei.br", "123456789", "SP", "player", "10000", "123", "1", "test@fei.br", "123456", "10", "ateste","3","2", "ateste", "0", "0", "asd", "sunday"); 
     }
     
     @After
