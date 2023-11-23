427af9a4918d6370db64910fa0669a52e1d9d64d
Rcarraro <carrarorafa@gmail.com>
2023-11-22 23:21:55 -0300

    teste

--- GOFO_Implementation/src/test/java/US02_2Test.java
+++ GOFO_Implementation/src/test/java/US02_2Test.java
@@ -13,7 +13,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 
-public class US03_2Test {
+public class US02_2Test {
 
     private final InputStream originalSystemIn = System.in;
     private final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
