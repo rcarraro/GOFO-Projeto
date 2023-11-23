36502c83c2562880117ad5d3f60cdebb22bc5df6
Rcarraro <carrarorafa@gmail.com>
2023-11-22 23:04:38 -0300

    teste

--- GOFO_Implementation/src/test/java/US07_1Test.java
+++ GOFO_Implementation/src/test/java/US07_1Test.java
@@ -75,9 +75,10 @@ public class US07_1Test {
         systemIn71.provideLines("2", "test", "Silva","123", "123", "test@fei.br", "123456789", "SP", "player", "10000", "123", "1", "test@fei.br", "123", "7", "playground owner", "testepla@fei.br", "tem um problema com o playground ateste", "12","1", "admin@gmail.com", "123", "5", "2", "ateste", "3", "ateste","yes","6", "3"); 
         sistema.accountMenu();
     }
-    @After
-    public void restoreSystemInputOutput() {
-        System.setIn(originalSystemIn);
-        systemIn71.provideLines("2");
-    }    
+    
+    // @After
+    // public void restoreSystemInputOutput() {
+    //     System.setIn(originalSystemIn);
+    //     systemIn71.provideLines("2");
+    // }    
 }
