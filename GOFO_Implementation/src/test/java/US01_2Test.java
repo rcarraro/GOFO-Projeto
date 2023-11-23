d0aa6f95fef1dd84a4f632de5089932aaf279d8d
Rcarraro <carrarorafa@gmail.com>
2023-11-22 23:20:31 -0300

    teste

--- GOFO_Implementation/src/test/java/US01_2Test.java
+++ GOFO_Implementation/src/test/java/US01_2Test.java
@@ -1,44 +1,44 @@
-// import codigo.UI.SystemUI;
-// import org.junit.After;
-// import org.junit.Before;
-// import org.junit.Test;
-// import org.junit.Rule;
-// import org.junit.contrib.java.lang.system.TextFromStandardInputStream;
-// import org.junit.contrib.java.lang.system.SystemOutRule;
-// import org.junit.contrib.java.lang.system.ExpectedSystemExit;
-// import java.io.InputStream;
+import codigo.UI.SystemUI;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Rule;
+import org.junit.contrib.java.lang.system.TextFromStandardInputStream;
+import org.junit.contrib.java.lang.system.SystemOutRule;
+import org.junit.contrib.java.lang.system.ExpectedSystemExit;
+import java.io.InputStream;
 
-// import static org.junit.Assert.assertEquals;
-// import static org.junit.Assert.assertThrows;
-// import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
 
-// public class US01_2Test {
+public class US01_2Test {
 
-//     private final InputStream originalSystemIn = System.in;
-//     private final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
+    private final InputStream originalSystemIn = System.in;
+    private final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
 
-//     @Rule
-//     public final TextFromStandardInputStream systemIn = TextFromStandardInputStream.emptyStandardInputStream();
+    @Rule
+    public final TextFromStandardInputStream systemIn = TextFromStandardInputStream.emptyStandardInputStream();
 
-//     @Rule
-//     public final ExpectedSystemExit exit = ExpectedSystemExit.none();
+    @Rule
+    public final ExpectedSystemExit exit = ExpectedSystemExit.none();
 
-//     private SystemUI sistema;
+    private SystemUI sistema;
    
-//     @Before
-//     public void setUp() {
-//         sistema = new SystemUI();
-//     }
+    @Before
+    public void setUp() {
+        sistema = new SystemUI();
+    }
 
-//     @Test
-//     public void testaccountMenuERRO() {
-//         exit.expectSystemExitWithStatus(0);
-//         systemIn.provideLines("2","123","teste","123","Silva","123","#####","testefei.edu.br","teste@fei.edu.br", "asd","123456789","1d12f1ad2", "player", "123", "3", "3");
-//         sistema.accountMenu();
-//     }
+    @Test
+    public void testaccountMenuERRO() {
+        exit.expectSystemExitWithStatus(0);
+        systemIn.provideLines("2","123","teste","123","Silva","123","#####","testefei.edu.br","teste@fei.edu.br", "asd","123456789","1d12f1ad2", "player", "123", "3", "3");
+        sistema.accountMenu();
+    }
     
-//     @After
-//     public void restoreSystemInputOutput() {
-//         System.setIn(originalSystemIn);
-//     }
-// }
+    @After
+    public void restoreSystemInputOutput() {
+        System.setIn(originalSystemIn);
+    }
+}
--- GOFO_Implementation/src/test/java/US02_2Test.java
+++ GOFO_Implementation/src/test/java/US02_2Test.java
@@ -1,48 +1,48 @@
-// import codigo.UI.SystemUI;
-// import codigo.System.Playground;
-// import org.junit.After;
-// import org.junit.Before;
-// import org.junit.Test;
-// import org.junit.Rule;
-// import org.junit.contrib.java.lang.system.TextFromStandardInputStream;
-// import org.junit.contrib.java.lang.system.SystemOutRule;
-// import org.junit.contrib.java.lang.system.ExpectedSystemExit;
-// import java.io.InputStream;
+import codigo.UI.SystemUI;
+import codigo.System.Playground;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Rule;
+import org.junit.contrib.java.lang.system.TextFromStandardInputStream;
+import org.junit.contrib.java.lang.system.SystemOutRule;
+import org.junit.contrib.java.lang.system.ExpectedSystemExit;
+import java.io.InputStream;
 
-// import static org.junit.Assert.assertEquals;
-// import static org.junit.Assert.assertThrows;
-// import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
 
-// public class US03_2Test {
+public class US03_2Test {
 
-//     private final InputStream originalSystemIn = System.in;
-//     private final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
+    private final InputStream originalSystemIn = System.in;
+    private final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
 
-//     @Rule
-//     public final TextFromStandardInputStream systemIn = TextFromStandardInputStream.emptyStandardInputStream();
+    @Rule
+    public final TextFromStandardInputStream systemIn = TextFromStandardInputStream.emptyStandardInputStream();
 
-//     @Rule
-//     public final ExpectedSystemExit exit = ExpectedSystemExit.none();
+    @Rule
+    public final ExpectedSystemExit exit = ExpectedSystemExit.none();
 
-//     private SystemUI sistema;
-//     private Playground playground;
+    private SystemUI sistema;
+    private Playground playground;
    
-//     @Before
-//     public void setUp() {
-//         sistema = new SystemUI();
-//         playground = new Playground();
-//         playground.setName("teste");
-//     }
+    @Before
+    public void setUp() {
+        sistema = new SystemUI();
+        playground = new Playground();
+        playground.setName("teste");
+    }
 
-//     @Test
-//     public void testaccountMenu() {
-//         exit.expectSystemExitWithStatus(0);
-//         systemIn.provideLines("2", "test", "Silva","123", "123456", "test@fei.br", "123456789", "SP", "playground owner", "10000", "123", "1", "test@fei.br", "123456", "1", "", "", "", "100", "", "available", "", "0", "", "7", "", "10", "8", "3");
-//         sistema.accountMenu();
-//     }
+    @Test
+    public void testaccountMenu() {
+        exit.expectSystemExitWithStatus(0);
+        systemIn.provideLines("2", "test", "Silva","123", "123456", "test@fei.br", "123456789", "SP", "playground owner", "10000", "123", "1", "test@fei.br", "123456", "1", "", "", "", "100", "", "available", "", "0", "", "7", "", "10", "8", "3");
+        sistema.accountMenu();
+    }
     
-//     @After
-//     public void restoreSystemInputOutput() {
-//         System.setIn(originalSystemIn);
-//     }
-// }
+    @After
+    public void restoreSystemInputOutput() {
+        System.setIn(originalSystemIn);
+    }
+}
