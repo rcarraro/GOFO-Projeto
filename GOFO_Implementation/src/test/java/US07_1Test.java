a65d3eadf05ff348fc29c209bae642967f1bf672
Rcarraro <carrarorafa@gmail.com>
2023-11-22 21:32:30 -0300

    teste

--- GOFO_Implementation/src/test/java/US07_1Test.java
+++ GOFO_Implementation/src/test/java/US07_1Test.java
@@ -66,7 +66,7 @@ public class US07_1Test {
         sistema.theOwners.add(playown);
     }
     
-    @Test
+    @Test 
     public void testUS07_1Test() {
         systemIn.provideLines("2", "test", "Silva","123", "123", "test@fei.br", "123456789", "SP", "player", "10000", "123", "1", "test@fei.br", "123", "7", "playground owner", "testepla@fei.br", "tem um problema com o playground ateste", "12","1", "admin@gmail.com", "123", "5", "2", "ateste", "3", "ateste","yes","6"); 
         assertThrows(NoSuchElementException.class, () -> {
