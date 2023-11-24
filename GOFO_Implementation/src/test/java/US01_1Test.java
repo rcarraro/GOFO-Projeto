d938a4eb0360a6a26fb16b99d798abe4f639a363
Rcarraro <carrarorafa@gmail.com>
2023-11-23 23:37:42 -0300

    teste

--- GOFO_Implementation/src/test/java/US01_1Test.java
+++ GOFO_Implementation/src/test/java/US01_1Test.java
@@ -35,6 +35,7 @@ public class US01_1Test {
         exit.expectSystemExitWithStatus(0);
         systemIn.provideLines("2","Kleber","Silva","123","123456","teste@fei.edu.br", "123456789", "SP", "player", "123", "3", "3");
         sistema.accountMenu();
+        assertEquals("teste@fei.edu.br", sistema.thePlayers.get(0).getEmail());
     }
     
     @After
